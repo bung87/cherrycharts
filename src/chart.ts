a24243192da27b075263bac566dd1e29168d3ac9
zhoupeng <zhoupeng@xjgreat.com>
2018-07-16 19:33:56 +0800

    refactor chart.ts

--- src/chart.ts
+++ src/chart.ts
@@ -1,7 +1,7 @@
 import Director from './director'
 import { Object3D, Vector2 } from 'three'
 import { ISize, IRect } from './interfaces'
-import { defaultsDeep,merge } from 'lodash'
+import { defaultsDeep, merge } from 'lodash'
 import optimizedResize from './interactions/optimized-resize'
 import * as themes from './themes'
 import * as d3time from 'd3-time'
@@ -30,7 +30,7 @@ class Chart extends Object3D implements IChart, IChartInteractable {
   labelFormat
   labelInterval
   _title
-  
+
   protected get size(): ISize {
     return this._size
   }
@@ -44,13 +44,13 @@ class Chart extends Object3D implements IChart, IChartInteractable {
   protected timeStart
   protected timeEnd
   private _dataSource
-  private _plotOptions;
+  private _plotOptions
   private _customPlotOptions = {}
   public get plotOptions() {
-    return this._plotOptions;
+    return this._plotOptions
   }
   public set plotOptions(value) {
-    this._plotOptions = {...value};
+    this._plotOptions = { ...value }
   }
   public get mainRect(): IRect {
     return this._mainRect
@@ -65,20 +65,8 @@ class Chart extends Object3D implements IChart, IChartInteractable {
   public set dataSource(value) {
     this._dataSource = [...value]
   }
-  private _director: Director
-  protected get director(): Director {
-    return this._director
-  }
-  protected set director(value: Director) {
-    this._director = value
-  }
-  private _useTimeRange: boolean = false
-  protected get useTimeRange(): boolean {
-    return this._useTimeRange
-  }
-  protected set useTimeRange(value: boolean) {
-    this._useTimeRange = value
-  }
+  private director: Director
+  private useTimeRange: boolean = false
 
   private _size: ISize
   private isResponsive: boolean
@@ -102,88 +90,32 @@ class Chart extends Object3D implements IChart, IChartInteractable {
     }
   }
 
-  updateMainRect(size?: ISize) {
-    let theSize = size ? size : this.size
-    this.size = { ...theSize }
-    this.mainRect.width = this.size.width - this.mainRect.left - this.mainRect.right
-    this.mainRect.height = this.size.height - this.mainRect.top - this.mainRect.bottom
-  }
-
-  renderTo(container: Element) {
-    this.container = container
-    this.director = new Director(container)
-    this.size = this.director.size
-    this.director.scene.add(this)
-    this.init()
-    this.render()
-    return this
-  }
-
-  makeCopy() {
-    // dirty work
-    let obj = Object.assign({}, this)
-    obj.container = null
-    obj.dataSource = []
-    obj.dataProcessed = false
-    obj.director = null
-    obj.size = null
-    obj.children = []
-    delete obj.uuid
-    delete obj.position
-    delete obj.rotation
-    delete obj.quaternion
-    delete obj.scale
-    const theClass = Object.getPrototypeOf(this).constructor
-    let a = new theClass()
-    Object.assign(a, obj)
-    return a
-  }
-
   public setOptions(value) {
     this.options = value
     return this
   }
 
-  public timeRange(start, end, unit: TimeUnit, interval = 1) {
-    this.useTimeRange = true
-    this.timeStart = start
-    this.timeEnd = end
-    this.xUnit = d3time[`time${capitalize(unit)}`]
-    this.xInterval = interval
-    switch (unit) {
-      case 'day':
-        this.xFormat = '%b %d'
-        break
-    }
-    return this
+  public setPlotOptions(value) {
+    merge(this._customPlotOptions, value)
   }
 
-  public xLabel(unit: TimeUnit, interval = 1) {
-    this.labelUnit = d3time[`time${capitalize(unit)}`]
-    this.labelInterval = interval
-    switch (unit) {
-      case 'month':
-        this.labelFormat = '%B'
-        break
+  public populateOptions() {
+    if (typeof this.options.theme === 'string') {
+      let themeName = this.options.theme
+      this.options.theme = themes[themeName]
+    } else {
+      defaultsDeep(this.options.theme, themes[defualtTheme])
     }
-    return this
+    this.plotOptions = this.getGlobalPlotOptions()
+    merge(this.plotOptions, this._customPlotOptions)
   }
 
-  addTooltip() {
-    this.tooltip = document.createElement('div')
-    this.tooltip.className = 'cherrycharts-tooltip'
-    this.container.appendChild(this.tooltip)
-    this.tooltip.onmouseout = this.tooltip.onmouseleave = this.onMouseLeaveTooltip.bind(this)
+  public build(data) {
+    throw new Error('Method not implemented.')
   }
 
-  init() {
-    this.isResponsive = this.getResponsive()
-    if (this.isResponsive) {
-      optimizedResize.add(this.onResize.bind(this))
-    }
-
-    this.addTooltip()
-    this.bindingEvents()
+  public draw(): void {
+    throw new Error('Method not implemented.')
   }
 
   title(text?: String) {
@@ -227,70 +159,114 @@ class Chart extends Object3D implements IChart, IChartInteractable {
     this.tooltip.style.display = 'block'
   }
 
-  bindingEvents() {
-    throw new Error('Method not implemented.')
-  }
-
-  populateOptions() {
-    if (typeof this.options.theme === 'string') {
-      let themeName = this.options.theme
-      this.options.theme = themes[themeName]
-    } else {
-      defaultsDeep(this.options.theme, themes[defualtTheme])
-    }
-    this.plotOptions = this.getGlobalPlotOptions()
-    merge(this.plotOptions,this._customPlotOptions)
-  }
-
-  getGlobalPlotOptions() {
-    let chartType = this.type.toLowerCase()
-    let index = chartType.indexOf('chart')
-    if (index !== -1) {
-      chartType = chartType.substring(0, index)
-      return this.options.theme.plotOptions[chartType]
-    }
-  }
-
-  setPlotOptions(value){
-    merge(this._customPlotOptions,value)
-  }
-
-  datum(data) {
+  public datum(data) {
     this.dataSource = data
-
     return this
   }
 
-  build(data) {
-    throw new Error('Method not implemented.')
-  }
-
-  draw(): void {
-    throw new Error('Method not implemented.')
-  }
-
-  updateSize(size: ISize): void {
-    throw new Error('Method not implemented.')
-  }
-
-  clearThree(obj) {
-    while (obj.children.length > 0) {
-      this.clearThree(obj.children[0])
-      obj.remove(obj.children[0])
-    }
-    if (obj.geometry) obj.geometry.dispose()
-    if (obj.material) obj.material.dispose()
-    if (obj.texture) obj.texture.dispose()
-  }
-
-  redraw(): void {
-    this.clearThree(this)
-    // this.updateMatrix()
-    // this.updateMatrixWorld(true)
+  public render() {
+    this.populateOptions()
+    this.updateMainRect()
+    this.drawCommon()
+    this.build(this.dataSource)
+    this.dataProcessed = true
     this.draw()
+    this.director._render()
   }
 
-  drawTitle() {
+  public renderTo(container: Element) {
+    this.container = container
+    this.director = new Director(container)
+    this.size = this.director.size
+    this.director.scene.add(this)
+    this.init()
+    this.render()
+    return this
+  }
+
+  public makeCopy() {
+    // dirty work
+    let obj = Object.assign({}, this)
+    obj.container = null
+    obj.dataSource = []
+    obj.dataProcessed = false
+    obj.director = null
+    obj.size = null
+    obj.children = []
+    delete obj.uuid
+    delete obj.position
+    delete obj.rotation
+    delete obj.quaternion
+    delete obj.scale
+    const theClass = Object.getPrototypeOf(this).constructor
+    let a = new theClass()
+    Object.assign(a, obj)
+    return a
+  }
+
+  public timeRange(start, end, unit: TimeUnit, interval = 1) {
+    this.useTimeRange = true
+    this.timeStart = start
+    this.timeEnd = end
+    this.xUnit = d3time[`time${capitalize(unit)}`]
+    this.xInterval = interval
+    switch (unit) {
+      case 'day':
+        this.xFormat = '%b %d'
+        break
+    }
+    return this
+  }
+
+  public xLabel(unit: TimeUnit, interval = 1) {
+    this.labelUnit = d3time[`time${capitalize(unit)}`]
+    this.labelInterval = interval
+    switch (unit) {
+      case 'month':
+        this.labelFormat = '%B'
+        break
+    }
+    return this
+  }
+
+  public bindingEvents() {
+    throw new Error('Method not implemented.')
+  }
+
+  public updateSize(size: ISize): void {
+    throw new Error('Method not implemented.')
+  }
+
+  private addTooltip() {
+    this.tooltip = document.createElement('div')
+    this.tooltip.className = 'cherrycharts-tooltip'
+    this.container.appendChild(this.tooltip)
+    this.tooltip.onmouseout = this.tooltip.onmouseleave = this.onMouseLeaveTooltip.bind(this)
+  }
+
+  private init() {
+    this.isResponsive = this.getResponsive()
+    if (this.isResponsive) {
+      optimizedResize.add(this.onResize.bind(this))
+    }
+    this.addTooltip()
+    this.bindingEvents()
+  }
+
+  private updateMainRect(size?: ISize) {
+    let theSize = size ? size : this.size
+    this.size = { ...theSize }
+    this.mainRect.width = this.size.width - this.mainRect.left - this.mainRect.right
+    this.mainRect.height = this.size.height - this.mainRect.top - this.mainRect.bottom
+  }
+
+  private drawCommon() {
+    if (this._title) {
+      this.drawTitle()
+    }
+  }
+
+  private drawTitle() {
     let style = this.options.theme.title.style
     let position = this.options.theme.title.position
     let horizontal = parseInt(position.x, 10) / 100
@@ -301,21 +277,30 @@ class Chart extends Object3D implements IChart, IChartInteractable {
     this.add(label)
   }
 
-  drawCommon() {
-    if (this._title) {
-      this.drawTitle()
+  private clearThree(obj) {
+    while (obj.children.length > 0) {
+      this.clearThree(obj.children[0])
+      obj.remove(obj.children[0])
     }
+    if (obj.geometry) obj.geometry.dispose()
+    if (obj.material) obj.material.dispose()
+    if (obj.texture) obj.texture.dispose()
   }
 
-  render() {
-    this.populateOptions()
-    this.updateMainRect()
-    this.drawCommon()
-    this.build(this.dataSource)
-
-    this.dataProcessed = true
+  private redraw(): void {
+    this.clearThree(this)
+    // this.updateMatrix()
+    // this.updateMatrixWorld(true)
     this.draw()
-    this.director._render()
+  }
+
+  private getGlobalPlotOptions() {
+    let chartType = this.type.toLowerCase()
+    let index = chartType.indexOf('chart')
+    if (index !== -1) {
+      chartType = chartType.substring(0, index)
+      return this.options.theme.plotOptions[chartType]
+    }
   }
 
   private getResponsive() {
